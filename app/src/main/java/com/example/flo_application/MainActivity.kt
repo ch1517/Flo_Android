3077167181810de01388b178f738108dd95e08a6
SecondWind <25702775+ch1517@users.noreply.github.com>
2021-04-13 15:36:06 +0900

    가사 전체화면 구현

--- app/src/main/java/com/example/flo_application/MainActivity.kt
+++ app/src/main/java/com/example/flo_application/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.flo_application
 
 import android.app.Activity
-import android.graphics.drawable.Drawable
+import android.content.Intent
 import android.media.MediaPlayer
 import android.net.Uri
 import android.os.AsyncTask
@@ -20,27 +20,26 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
 import java.net.URL
+import java.text.FieldPosition
 
 class MainActivity : AppCompatActivity() {
     private val urlInfo = "https://grepp-programmers-challenges.s3.ap-northeast-2.amazonaws.com/2020-flo/song.json"
+    private val mp: MediaPlayer = MediaPlayer()
+    private lateinit var mContext:Activity
+    private lateinit var runTread:Runnable
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        RestAPITask(this).execute(urlInfo)
+        mContext=this
+        RestAPITask().execute(urlInfo)
     }
 
-    class RestAPITask : AsyncTask<String, JSONObject, JSONObject>{
-        private var mContext:Activity
-        private lateinit var mp: MediaPlayer
-        constructor(context:Activity) {
-            mContext =context
-        }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+    inner class RestAPITask : AsyncTask<String, JSONObject, JSONObject>(){
         override fun onPreExecute() {
             super.onPreExecute()
-            mp = MediaPlayer()
-
-
         }
         override fun doInBackground(vararg urlStr: String): JSONObject? {
             var jsonObject: JSONObject? = null
@@ -60,7 +59,6 @@ class MainActivity : AppCompatActivity() {
                     buffered.close()
                     conn.disconnect()
                     jsonObject = JSONObject(content.toString())
-                    Log.d("File",Uri.parse(jsonObject.getString("file") as String).toString())
 
                     mp.setDataSource(jsonObject.getString("file"))
                     mp.prepare()
@@ -80,27 +78,37 @@ class MainActivity : AppCompatActivity() {
                 mContext.singerTxt.text = result.getString("singer") as String
                 mContext.albumTxt.text = result.getString("album") as String
                 mContext.titleTxt.text = result.getString("title") as String
+                var lyrics = result.getString("lyrics").split("[","]").toMutableList()
+                lyrics.removeAt(0)
 
-                settingSeekBar()
+                for (i in 0..lyrics.size-1 step 1){
+                    if(i%2==0){
+                        //시간 초로 변환
+                        var str = lyrics[i].split(":")
+                        lyrics[i]=(Integer.parseInt(str[0])*60000+Integer.parseInt(str[1])*1000+Integer.parseInt(str[2])).toString()
+                    }else{
+                        // 공백문자 제거
+                        lyrics[i] = lyrics[i].substring(0,lyrics[i].length-1)
+                    }
+                }
+
+                mContext.totalLyricBtn.setOnClickListener {
+                    val intent = Intent(mContext,TotalLyricActivity::class.java)
+                    intent.putExtra("lyrics",lyrics.toTypedArray())
+                    mContext.startActivityForResult(intent,1)
+                }
+                settingSeekBar(lyrics)
                 Glide.with(mContext).load(alumImgURL)
                 .transform(CenterCrop(), RoundedCorners(30)).into(mContext.albumImg);
             }
         }
-        fun settingSeekBar(){
+
+        fun settingSeekBar(lyrics: List<String>){
+            var handler:Handler = Handler()
             if (mp!=null){
                 mContext.seekBar.max = mp.duration
-
-                // 재생 시 seekBar update
-                var handler:Handler = Handler()
-                mContext.runOnUiThread(object : Runnable {
-                    override fun run() {
-                        Thread.sleep(100)
-                        if (mp.isPlaying){ // 재생 중일 때만 seekbar update
-                            mContext.seekBar.setProgress(mp.getCurrentPosition())
-                        }
-                        handler.postDelayed(this,1000)
-                    }
-                })
+                runTread=runnable(handler,lyrics)
+                mContext.runOnUiThread(runTread)
             }
             mp.setOnPreparedListener(object:MediaPlayer.OnPreparedListener{
                 override fun onPrepared(p0: MediaPlayer?) {
@@ -130,6 +138,29 @@ class MainActivity : AppCompatActivity() {
                     mp.pause()
                     mContext.startBtn.background = mContext.getDrawable(R.drawable.play)
                 }
+            }
+        }
+
+        fun runnable(handler:Handler,lyrics:List<String>): Runnable {
+            return object : Runnable {
+                override fun run() {
+                    if (mp.isPlaying){ // 재생 중일 때만 seekbar update
+                        mContext.seekBar.setProgress(mp.getCurrentPosition())
+                        settingLyrics(mp.getCurrentPosition(),lyrics)
+                    }
+                    handler.postDelayed(this,200)
+                }
+            }
+        }
+        fun settingLyrics(duration: Int, lyrics: List<String>){
+            var i = 0
+            var index = -1
+            while(i<lyrics.size-1 && duration>Integer.parseInt(lyrics[i])){
+                index=i
+                i+=2
+            }
+            if(index>-1 && index<lyrics.size-1){
+                mContext.lyricsTxt.text = lyrics[index+1]
             }
         }
     }
